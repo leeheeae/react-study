2702004dbdf09cf588f6255b348fe968b5872c68
leeheeae <chromeee@daum.net>
2021-07-23 22:36:37 +0900

    -

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import { Fragment }from 'react';
+import './App.css';
 
 function App() {
+  const name = "리액트"
+  const number = 0;
+  const name2 = undefined;
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '48px',
+    fontWeight: 'bold',
+    padding: 16
+  }
   return (
-    <Fragment>
-      <h1>리액트 안녕!</h1>    
-      <h2>잘 작동하니?</h2>
-    </Fragment>
+    <>
+      {/* name === '리액트' ? (<h1>리액트입니다.</h1>: null) */}
+      {name === '리액트' && <h1>리액트입니다.</h1>}
+      {number && <div>내용</div>}
+      {name2 || '리액트'}
+
+      <div style={style}>{name}</div>
+
+      <div style={{
+        backgroundColor: 'black',
+        color: 'aqua',
+        fontSize: '48px',
+        fontWeight: 'bold',
+        padding: 16
+      }}
+      >
+        {name}
+      </div>
+
+      <div className="react">{name}</div>
+
+      <input />
+    </>
   );
+  //return <div style={style}>{name}</div>
+  // return name2 || '값이 undefined 입니다';
 }
 
 export default App;
