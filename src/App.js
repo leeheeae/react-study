cc0c23780d465d8e9f2f09132fafd027ea4f6da0
leeheeae <chromeee@daum.net>
2021-07-23 22:59:38 +0900

    -

--- src/App.js
+++ src/App.js
@@ -1,44 +1,8 @@
-import { Fragment }from 'react';
-import './App.css';
+import React from 'react';
+import MyComponent from './MyComponent';
 
-function App() {
-  const name = "리액트"
-  const number = 0;
-  const name2 = undefined;
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: '48px',
-    fontWeight: 'bold',
-    padding: 16
-  }
-  return (
-    <>
-      {/* name === '리액트' ? (<h1>리액트입니다.</h1>: null) */}
-      {name === '리액트' && <h1>리액트입니다.</h1>}
-      {number && <div>내용</div>}
-      {name2 || '리액트'}
-
-      <div style={style}>{name}</div>
-
-      <div style={{
-        backgroundColor: 'black',
-        color: 'aqua',
-        fontSize: '48px',
-        fontWeight: 'bold',
-        padding: 16
-      }}
-      >
-        {name}
-      </div>
-
-      <div className="react">{name}</div>
-
-      <input />
-    </>
-  );
-  //return <div style={style}>{name}</div>
-  // return name2 || '값이 undefined 입니다';
-}
+const App = () => {
+  return <MyComponent>리액트</MyComponent>;
+};
 
 export default App;
