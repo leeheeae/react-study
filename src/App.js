5923f88ac4cc1fb339b7c7866d0d27bca23aee5c
leeheeae <chromeee@daum.net>
2021-07-24 22:58:33 +0900

    -

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import React, { Component } from 'react';
-import Counter from './counter';
-import Say from './Say';
-import EventPractice from './EventPractice';
-import EventPractice2 from './EventPractice2';
-import EventPractice3 from './EventPractice3';
-import EventPractice4 from './EventPractice4';
-import ValidationSample from './ValidationSample';
-import ScrollBox from './ScrollBox';
+import IterationSample from './IterationSample';
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        {/* <Counter />
-      <Say />
-      <EventPractice />
-      <EventPractice2 />
-      <EventPractice3 />
-      <EventPractice4 />
-      <ValidationSample /> */}
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <IterationSample />
+    </>
+  );
+};
 
 export default App;
