3504c5df927b98f95cbc165496ae403f3ee8df4e
leeheeae <chromeee@daum.net>
2021-07-24 23:43:23 +0900

    -

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
-import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
 
-const App = () => {
-  return (
-    <>
-      <IterationSample />
-    </>
-  );
-};
+//랜덤 색상 생성
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+class App extends Component {
+  state = {
+    color: '#000000',
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
+  render() {
+    return (
+      <>
+        {this.props.missing.value}
+        <button onClick={this.handleClick}>랜덤색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </>
+    );
+  }
+}
 
 export default App;
