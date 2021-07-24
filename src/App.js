b485046da3c16b362bc7ff912c138e7f4a452c5d
leeheeae <chromeee@daum.net>
2021-07-24 21:39:18 +0900

    -

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Counter from './counter';
 import Say from './Say';
+import EventPractice from './EventPractice';
+import EventPractice2 from './EventPractice2';
+import EventPractice3 from './EventPractice3';
 
 const App = () => {
   return (
     <>
       <Counter />
       <Say />
+      <EventPractice />
+      <EventPractice2 />
+      <EventPractice3 />
     </>
   );
 };
