7ea9605397fd52358daab9cec39d0a2733be472a
leeheeae <chromeee@daum.net>
2021-07-24 20:45:49 +0900

    -

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import MyComponent from './MyComponent';
 import Counter from './counter';
+import Say from './Say';
 
 const App = () => {
   return (
-    // <MyComponent name={'React'} favoriteNumber={1}>
-    //   리액트
-    // </MyComponent>
-    <Counter />
+    <>
+      <Counter />
+      <Say />
+    </>
   );
 };
 
--- src/counter.js
+++ src/counter.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
 
 class counter extends Component {
-  constructor(props) {
-    super(props);
-    //state의 초기값 설정하기
-    this.state = {
-      number: 0,
-      fixeNumber: 0,
-    };
-  }
+  state = {
+    number: 0,
+    fixeNumber: 0,
+  };
   render() {
     const { number, fixeNumber } = this.state; //state를 조회할 때는 this.state로 조회합니다.
     return (
       <div>
         <h1>{number}</h1>
         <h2>{fixeNumber}</h2>
-        <button onClick={() => this.setState({ number: number + 1 })}>
+        <button
+          onClick={() => {
+            this.setState((prevState) => {
+              return {
+                number: prevState.number + 1,
+              };
+            });
+            this.setState((prevState) => ({
+              number: prevState.number + 1,
+            }));
+            this.setState(
+              {
+                number: number + 1,
+              },
+              () => {
+                console.log('방금 setState가 호출되었습니다.');
+                console.log(this.state);
+              }
+            );
+          }}
+        >
           +1
         </button>
       </div>
