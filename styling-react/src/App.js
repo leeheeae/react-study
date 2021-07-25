efb1ec55e88a1415de1508e8cdbfd912bbdf6579
leeheeae <chromeee@daum.net>
2021-07-25 15:42:49 +0900

    -

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CSSModule from './CSSModule';
+import StyledComponent from './StyledComponent';
 
 const App = () => {
   return (
     <div>
       <CSSModule />
+      <StyledComponent />
     </div>
   );
 };
