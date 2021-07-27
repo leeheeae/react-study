aa394b4607c24982755cb2d010e21ecd000d1588
leeheeae <chromeee@daum.net>
2021-07-27 21:31:16 +0900

    immer-tutorial

--- immer-tutorial/src/App.js
+++ immer-tutorial/src/App.js
@@ -1,4 +1,5 @@
 import React, { useRef, useCallback, useState } from 'react';
+import produce from 'immer';
 
 const App = () => {
   const nextId = useRef(1);
@@ -9,16 +10,14 @@ const App = () => {
   });
 
   //input 수정을 위한 함수
-  const onChange = useCallback(
-    (e) => {
-      const { name, value } = e.target;
-      setForm({
-        ...form,
-        [name]: [value],
-      });
-    },
-    [form]
-  );
+  const onChange = useCallback((e) => {
+    const { name, value } = e.target;
+    setForm(
+      produce((draft) => {
+        draft[name] = value;
+      })
+    );
+  }, []);
 
   //form 등록을 위한 함수
   const onSubmit = useCallback(
@@ -30,10 +29,16 @@ const App = () => {
         username: form.username,
       };
       // array의 새 항목 등록
-      setData({
-        ...data,
-        array: data.array.concat(info),
-      });
+      setData(
+        //   {
+        //   ...data,
+        //   array: data.array.concat(info),
+        // }
+
+        produce((draft) => {
+          draft.array.push(info);
+        })
+      );
 
       //form 초기화
       setForm({
@@ -43,19 +48,24 @@ const App = () => {
 
       nextId.current += 1;
     },
-    [data, form.name, form.username]
+    [form.name, form.username]
   );
 
   //항목을 삭제하는 함수
-  const onRemove = useCallback(
-    (id) => {
-      setData({
-        ...data,
-        array: data.array.filter((info) => info.id !== id),
-      });
-    },
-    [data]
-  );
+  const onRemove = useCallback((id) => {
+    setData(
+      //   {
+      //   ...data,
+      //   array: data.array.filter((info) => info.id !== id),
+      // }
+      produce((draft) => {
+        draft.array.splice(
+          draft.array.findIndex((info) => info.id === id),
+          1
+        );
+      })
+    );
+  }, []);
 
   return (
     <>
