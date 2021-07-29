7f108b39d6b65a26e9f5cc97231ed5530a9d3306
leeheeae <chromeee@daum.net>
2021-07-29 22:14:32 +0900

    axios

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,40 +1,16 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState, useCallback } from 'react';
+import NewsList from './components/NewsList';
+import Categories from './components/Categories';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  // const onClick = () => {
-  //   axios
-  //     .get('https://jsonplaceholder.typicode.com/todos/1')
-  //     .then((response) => {
-  //       setData(response.data);
-  //     });
-  // };
-
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos/1',
-      );
-      setData(response.data);
-    } catch (err) {
-      console.error(err);
-    }
-  };
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback((category) => setCategory(category), []);
 
   return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
+    <>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
+    </>
   );
 };
 
---
+++ news-viewer/src/components/NewsList.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import axios from 'axios';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 2rem auto 0;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const NewsList = ({ category }) => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    //async를 사용하는 함수 따로 선언
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const query = category === 'all' ? '' : `&category=${category}`;
+        const response = await axios.get(
+          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=4f46e736be174af89f54440638fe7956`,
+        );
+        setArticles(response.data.articles);
+      } catch (err) {
+        console.error(err);
+      }
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [category]);
+
+  //대기중일때
+  if (loading) {
+    return <NewsListBlock>대기중...</NewsListBlock>;
+  }
+
+  //아직 articles 값이 설정되지 않았을 때
+  if (!articles) {
+    return null;
+  }
+
+  //articles 값이 유효할 때
+  return (
+    <div>
+      <NewsListBlock>
+        {articles.map((article) => (
+          <NewsItem key={article.url} article={article} />
+        ))}
+      </NewsListBlock>
+    </div>
+  );
+};
+
+export default NewsList;
