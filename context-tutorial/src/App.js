9b2c495b41ac8b990077509de2f42c915ca1e1cc
leeheeae <chromeee@daum.net>
2021-08-03 21:44:30 +0900

    contexts-box

---
+++ context-tutorial/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ColorBox from './components/ColorBox';
+import ColorContext from './contexts/color';
+
+const App = () => {
+  return (
+    <ColorContext.Provider value={{ color: 'red' }}>
+      <ColorBox />
+    </ColorContext.Provider>
+  );
+};
+
+export default App;
