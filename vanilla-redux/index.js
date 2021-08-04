20888060bc9c035137f65899ebb5206ffe96cb71
leeheeae <chromeee@daum.net>
2021-08-04 21:22:05 +0900

    redux

--- vanilla-redux/index.js
+++ vanilla-redux/index.js
@@ -18,3 +18,28 @@ const initialState = {
   toggle: false,
   counter: 0,
 };
+
+// 리듀서 함수 정의
+// state가 undefined 일 때는 initialState를 기본값으로 사용
+function reducer(state = initialState, action) {
+  //action.type에 따라 다른 작업을 처리함
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state, //불변성을 유지해 주어야 합니다.
+        toggle: !state.toggle,
+      };
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + action.difference,
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+}
