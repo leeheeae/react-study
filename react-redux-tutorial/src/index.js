cb18b3c7d50975b08942a5b29d2d26da3883efef
leeheeae <chromeee@daum.net>
2021-08-07 21:31:50 +0900

    react-redux

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
