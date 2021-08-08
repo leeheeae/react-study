5fbf4750f6245bccf324f14e1753deac14901907
leeheeae <chromeee@daum.net>
2021-08-08 13:33:30 +0900

    useSelector, useDispatch 사용하기

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter
+      number={number}
+      onIncrease={() => dispatch(increase())}
+      onDecrease={() => dispatch(decrease())}
+    />
   );
 };
 
-export default connect(
-  (state) => ({
-    number: state.counter.number,
-  }),
-  (dispatch) =>
-    bindActionCreators(
-      {
-        increase,
-        decrease,
-      },
-      dispatch,
-    ),
-)(CounterContainer);
+export default CounterContainer;
