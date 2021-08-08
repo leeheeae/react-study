7e12023caee8e2088880a8a801742c1a206fa34c
leeheeae <chromeee@daum.net>
2021-08-08 19:56:42 +0900

    project

---
+++ react-project/blog-frontend/src/components/auth/AuthForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+/* 회원가입 또는 로그인 폼을 보여줌 */
+const AuthFormBlock = styled.div``;
+
+const AuthForm = () => {
+  return (
+    <div>
+      <AuthFormBlock>AuthFormBlock</AuthFormBlock>
+    </div>
+  );
+};
+
+export default AuthForm;
