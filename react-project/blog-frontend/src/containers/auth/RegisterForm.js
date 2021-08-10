78e1ac7f17f25a6a2950f11a8cafd0445a45c3ed
leeheeae <chromeee@daum.net>
2021-08-10 22:44:26 +0900

    회원가입연연결

--- react-project/blog-frontend/src/containers/auth/RegisterForm.js
+++ react-project/blog-frontend/src/containers/auth/RegisterForm.js
@@ -2,13 +2,16 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeField, initializeForm, register } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
+import { check } from '../../modules/user';
+import { withRouter } from 'react-router-dom';
 
-const RegisterForm = () => {
+const RegisterForm = ({ history }) => {
   const dispatch = useDispatch();
-  const { form, auth, authError } = useSelector(({ auth }) => ({
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.register,
     auth: auth.auth,
     authError: auth.authError,
+    user: user.user,
   }));
 
   //인풋 변경 이벤트 핸들러
@@ -47,8 +50,23 @@ const RegisterForm = () => {
     if (auth) {
       console.log('회원가입 성공');
       console.log(auth);
+      dispatch(check());
     }
-  });
+  }, [auth, authError, dispatch]);
+
+  useEffect(() => {
+    if (user) {
+      console.log('check API 성공');
+      console.log('user');
+    }
+  }, [user]);
+
+  //user값이 잘 설정되었는지 확인
+  useEffect(() => {
+    if (user) {
+      history.push('/'); //홈 화면으로 이동
+    }
+  }, [history, user]);
 
   return (
     <AuthForm
